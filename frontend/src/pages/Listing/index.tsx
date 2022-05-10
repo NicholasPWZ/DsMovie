1e55522c52e207bc821e9e1f2873dea6fdb066aa
NicholasPWZ <nicholcosta@hotmail.com>
2022-05-10 20:50:03 -0300

    movie card

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,6 +1,31 @@
+import MovieCard from "../../components/MovieCard";
 import Pagination from "../../components/Pagination";
 function Listing(){
     return(
-<Pagination/>    );
+        <>
+<Pagination/> 
+<div className="container">
+  <div className="row">
+    <div className="col-sm-4 col-lg-5 col-xl-6 mb-3">
+    <MovieCard/> 
+    </div>
+
+    <div className="col-sm-4 col-lg-5 col-xl-6 mb-3">
+    <MovieCard/> 
+    </div>  <div className="col-sm-4 col-lg-5 col-xl-6 mb-3">
+    <MovieCard/> 
+    </div>
+    <div className="col-sm-4 col-lg-5 col-xl-6 mb-3">
+    <MovieCard/> 
+    </div>
+    <div className="col-sm-4 col-lg-5 col-xl-6 mb-3">
+    <MovieCard/> 
+    </div>
+    
+  </div>
+</div>
+
+        </>
+  );
 }
 export default Listing;
