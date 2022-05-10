f455345c5da4b07a3c60e4e4a08831453492c2a1
NicholasPWZ <nicholcosta@hotmail.com>
2022-05-10 19:22:18 -0300

    navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-
+import Navbar from "./components/Navbar"; 
 
 function App() {
-  return (
-    <h1>Página principal</h1>
-  );
+  return(
+    <Navbar/>
+  )
 }
 
 export default App;
